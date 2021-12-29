aa5ea177bb2f6e290ec961640567cb1b4b1231ae
simondoesstuff <sw.simonwalker@gmail.com>
2021-12-29 10:09:45 -0700

    Started primary layout markup Bootstrap project set up

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,19 @@
-import '../styles/globals.scss'
-import {useEffect} from "react"; // bootstrap
+import '@/styles/global/styles.scss'
+import {useEffect} from "react";
+import NavFooterLayout from "../components/layouts/NavFooterLayout";
 
 function MyApp({ Component, pageProps }) {
+  // load the JS for bootstrap
+  // (the css is loaded through the global style)
   useEffect(() => {
     import("bootstrap/dist/js/bootstrap");
   }, []);
 
-  return <Component {...pageProps} />
+  return (
+    <NavFooterLayout>
+      <Component {...pageProps} />
+    </NavFooterLayout>
+  )
 }
 
 export default MyApp
