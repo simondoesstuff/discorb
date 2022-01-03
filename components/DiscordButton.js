e08586b11fcf899ec396d8107d6fb4e913c85881
simondoesstuff <sw.simonwalker@gmail.com>
2022-01-03 14:48:44 -0700

    new discord-themed components

---
+++ components/DiscordButton.js
@@ -0,0 +1,96 @@
+import React from 'react';
+import {Button, useTheme} from "@mui/material";
+
+function sxFrom(theme, variant) {
+  function specificSx() {
+    switch (variant) {
+      case 'white': return (
+        {
+          color: theme.discordPalette.black,
+          backgroundColor: theme.discordPalette.white,
+
+          '&:hover': {
+            color: theme.discordPalette.brand,
+            backgroundColor: theme.discordPalette.white,
+          },
+        }
+      )
+
+      case 'black': return (
+        {
+          color: theme.discordPalette.white,
+          backgroundColor: theme.discordPalette.gray,
+
+          '&:hover': {
+            backgroundColor: theme.discordPalette.lighterGray,
+          },
+        }
+      )
+
+      case 'blue': return (
+        {
+          color: theme.discordPalette.white,
+          backgroundColor: theme.discordPalette.brand,
+
+          '&:hover': {
+            backgroundColor: theme.discordPalette.lighterBrand,
+          },
+        }
+      )
+    }
+  }
+
+  // here we return the default style
+  // that rides above all the specific
+  // styles.
+  //
+  // These styles are merged
+  // (along with others)
+  // in the calling function.
+
+  return [
+    {
+      textTransform: 'initial',
+      transition: 'background-color, color, box-shadow',
+      transitionDuration: '.2s',
+      transitionTimingFunction: 'ease-in-out',
+
+      '&:hover': {
+        boxShadow: '0 8px 15px rgba(0,0,0,.2)'
+      }
+    },
+
+    specificSx()
+  ]
+}
+
+/**
+ * @param variant
+ *  Support variants: 'black', 'white', 'blue'
+ */
+function DiscordButton({
+                         children,
+                         sx,
+                         variant
+                       }) {
+
+  const theme = useTheme();
+
+  return (
+    <Button
+      disableRipple
+      disableElevation
+
+      sx={[
+        ...sxFrom(theme, variant),
+
+        // You cannot spread `sx` directly because `SxProps` (typeof sx) can be an array.
+        ...(Array.isArray(sx) ? sx : [sx]),
+      ]}
+    >
+      {children}
+    </Button>
+  );
+}
+
+export default DiscordButton;
--- components/DiscordLogo.js
+++ components/DiscordLogo.js
@@ -1,25 +1,60 @@
 import React from 'react';
-import {Stack, Typography} from "@mui/material";
+import {Box, Link as MuiLink} from "@mui/material";
+import NextLink from 'next/link'
 import DiscordLogoWithTextSvg from '@/public/discordSvgs/DiscordLogoWithText.svg'
 import DiscordLogoSvg from '@/public/discordSvgs/DiscordSymbol.svg'
 import Image from 'next/image'
 
-export function DiscordLogo({height, width}) {
+function LogoBox({src, height, width, black}) {
+  let innerLink = (
+    <MuiLink
+      underline="hover"
+      href='/'
+    >
+      <Image
+        src={src}
+        height={height}
+        width={width}
+      />
+    </MuiLink>
+  );
+
+  if (black) {
+    innerLink = (
+      <Box sx={{
+        filter: 'brightness(0)'
+      }}>
+        {innerLink}
+      </Box>
+    );
+  }
+
   return (
-    <Image
-      src={DiscordLogoSvg}
-      height={height}
-      width={width}
-    />
+    <NextLink
+      href={'/'}
+      alt="HomePage"
+      passRef
+    >
+      {innerLink}
+    </NextLink>
   );
 }
 
-export function DiscordLogoWithName({height= 34, width = 124}) {
+// todo does this need to pass the height & width props?
+export function DiscordLogo({black}) {
   return (
-    <Image
+    <LogoBox black={black} src={DiscordLogoSvg}/>
+  )
+}
+
+// these height and width parameters are not applied to the svg
+export function DiscordLogoWithName({height = '34px', width = '124px', black}) {
+  return (
+    <LogoBox
       src={DiscordLogoWithTextSvg}
       height={height}
-      width={width}
+      wdith={width}
+      black={black}
     />
   )
 }
