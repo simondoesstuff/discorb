7c2dfcf9eadd7f28cf0fdaf52cbb9d10a27c471a
simondoesstuff <sw.simonwalker@gmail.com>
2022-01-08 07:39:18 -0700

    minor clean-ups to header JS

--- components/layouts/NavFooterLayout/Header.js
+++ components/layouts/NavFooterLayout/Header.js
@@ -1,73 +1,70 @@
 import React from 'react';
-import {AppBar, Container, Link, Stack, Toolbar, Typography} from "@mui/material";
+import {AppBar, Box, Container, Link, Stack, Toolbar, Typography} from "@mui/material";
 import {DiscordLogoWithName} from "@/components/DiscordLogo";
 import DiscordButton from "@/components/DiscordButton";
 import NextLink from 'next/link'
 
+// todo looks terrible on error404 page
 function Header() {
   return (
-    <header>
-      <Toolbar>
-        {/*
-        This AppBar will only show when the page
-        is without a banner. Eg: Error404 Page.
-        Otherwise, the banner will cover it up.
-        */}
-        <AppBar
-          sx={{
-            position: 'absolute',
-            zIndex: -10,
-            bgcolor: theme => theme.palette.secondary.main,
-            width: '100%',
-            height: '100%'
-          }}
-        />
+    <Box sx={{
+      // position: 'absolute',
+      // top: 0,
+      // zIndex: 1,
+      // width: '100%'
+    }}
+    >
+      <Container sx={{
+        p: '1.5rem 0',
+        // margin: 'auto',
+      }}
+      >
+        <Stack
+          direction="row"
+          justifyContent="space-between"
+          alignItems="center"
+          width={'100%'}
+        >
+          {/*Discord Logo*/}
+          <DiscordLogoWithName/>
 
-        <Container sx={{p: '1.5rem 0'}}>
+          {/*Page Links*/}
           <Stack
             direction="row"
-            justifyContent="space-between"
-            alignItems="center"
-            width={'100%'}
+            gap={5}
           >
-            {/*Discord Logo*/}
-            <DiscordLogoWithName/>
-
-            {/*Page Links*/}
-            <Stack
-              direction="row"
-              gap={5}
-            >
-              {
-                // the values in this array refer to page routes
-                ['Download', 'Nitro', 'Safety', 'Support', 'Blog', 'Careers']
-                  .map((e, i) => (
-                    <NextLink
-                      key={i}
-                      href={`/${e}`}
-                      passHref
+            {
+              // the values in this array refer to page routes
+              ['Download', 'Nitro', 'Safety', 'Support', 'Blog', 'Careers']
+                .map((e, i) => (
+                  <NextLink
+                    key={i}
+                    href={`/${e}`}
+                    passHref
+                  >
+                    <Link
+                      color={theme => theme.palette.text.secondary}
+                      underline='hover'
                     >
-                      <Link
-                        color={theme => theme.palette.text.secondary}
-                        underline='hover'
+                      <Typography
+                        variant={'subtitle1'}
+                        fontWeight={600}
                       >
-                        <Typography variant={'subtitle1'}>
-                          {e}
-                        </Typography>
-                      </Link>
-                    </NextLink>
-                  ))
-              }
-            </Stack>
-
-            {/*Login Button*/}
-            <DiscordButton variant='white'>
-              Login
-            </DiscordButton>
+                        {e}
+                      </Typography>
+                    </Link>
+                  </NextLink>
+                ))
+            }
           </Stack>
-        </Container>
-      </Toolbar>
-    </header>
+
+          {/*Login Button*/}
+          <DiscordButton variant='white'>
+            Login
+          </DiscordButton>
+        </Stack>
+      </Container>
+    </Box>
   )
 }
 
