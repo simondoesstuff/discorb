14bf8668ba07e15f783e37ee596a036c869f8143
simondoesstuff <sw.simonwalker@gmail.com>
2022-01-12 18:52:58 -0700

    header is now responsive

--- components/layouts/NavFooterLayout/Header.js
+++ components/layouts/NavFooterLayout/Header.js
@@ -1,70 +1,98 @@
 import React from 'react';
-import {AppBar, Box, Container, Link, Stack, Toolbar, Typography} from "@mui/material";
+import {Box, Button, Container, Link, Stack, Typography, useMediaQuery} from "@mui/material";
+import MenuIcon from '@mui/icons-material/Menu';
 import {DiscordLogoWithName} from "@/components/DiscordLogo";
 import DiscordButton from "@/components/DiscordButton";
 import NextLink from 'next/link'
 
 // todo looks terrible on error404 page
 function Header() {
+  const onSmallScreen = useMediaQuery((theme => theme.breakpoints.down('md')));
+
   return (
-    <Box sx={{
-      // position: 'absolute',
-      // top: 0,
-      // zIndex: 1,
-      // width: '100%'
+    <Container sx={{
+      p: '1.5rem',
     }}
     >
-      <Container sx={{
-        p: '1.5rem 0',
-        // margin: 'auto',
-      }}
+      <Stack
+        direction="row"
+        justifyContent="space-between"
+        alignItems="center"
+        width={'100%'}
       >
-        <Stack
-          direction="row"
-          justifyContent="space-between"
-          alignItems="center"
-          width={'100%'}
-        >
-          {/*Discord Logo*/}
-          <DiscordLogoWithName/>
+        {/*Discord Logo*/}
+        <DiscordLogoWithName/>
 
-          {/*Page Links*/}
-          <Stack
-            direction="row"
-            gap={5}
-          >
-            {
-              // the values in this array refer to page routes
-              ['Download', 'Nitro', 'Safety', 'Support', 'Blog', 'Careers']
-                .map((e, i) => (
-                  <NextLink
-                    key={i}
-                    href={`/${e}`}
-                    passHref
-                  >
-                    <Link
-                      color={theme => theme.palette.text.secondary}
-                      underline='hover'
+        {/*Page Links
+            Does not render on small screens
+        */}
+        {
+          !onSmallScreen && (
+            <Stack
+              direction="row"
+              gap={5}
+            >
+              {
+                // the values in this array refer to page routes
+                ['Download', 'Nitro', 'Safety', 'Support', 'Blog', 'Careers']
+                  .map((e, i) => (
+                    <NextLink
+                      key={i}
+                      href={`/${e}`}
+                      passHref
                     >
-                      <Typography
-                        variant={'subtitle1'}
-                        fontWeight={600}
+                      <Link
+                        color={theme => theme.palette.text.secondary}
+                        underline='hover'
                       >
-                        {e}
-                      </Typography>
-                    </Link>
-                  </NextLink>
-                ))
-            }
-          </Stack>
+                        <Typography
+                          variant={'subtitle1'}
+                          fontWeight={600}
+                        >
+                          {e}
+                        </Typography>
+                      </Link>
+                    </NextLink>
+                  ))
+              }
+            </Stack>
+          )
+        }
 
+        <Box>
           {/*Login Button*/}
-          <DiscordButton variant='white'>
-            Login
-          </DiscordButton>
-        </Stack>
-      </Container>
-    </Box>
+          <LoginButton/>
+
+          {/*  Menu Stack Button
+          Only renders on small screens
+        */}
+          {onSmallScreen && (
+            <MenuButton/>
+          )}
+        </Box>
+      </Stack>
+    </Container>
+  )
+}
+
+function LoginButton() {
+  return (
+    <DiscordButton variant='white'>
+      Login
+    </DiscordButton>
+  )
+}
+
+function MenuButton() {
+  return (
+    <Button>
+      <MenuIcon
+        fontSize="large"
+        sx={{
+          color: theme => theme.discordPalette.white
+        }}
+      />
+    </Button>
   )
 }
 
